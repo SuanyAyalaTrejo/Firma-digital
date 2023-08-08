05027471d83b4075676d9d3bd1185ec2d8e8fac3
Suany Ayala Trejo <suamat10@gmail.com>
2023-08-08 12:45:48 -0600

    Agregar archivos de proyecto.

---
+++ Firma digital/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using SignaturePad.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Firma_digital
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+           Stream image = await VistadeFirma.GetImageStreamAsync(SignatureImageFormat.Jpeg) ;
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            VistadeFirma.Clear();
+        }
+    }
+}
